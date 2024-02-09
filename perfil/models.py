750186a89337c7e35d82fe5327bef47ddaa54b1e
Filipe <filipelisboaalves@gmail.com>
2024-02-09 12:35:04 -0300

    criando os models do app perfil

--- perfil/models.py
+++ perfil/models.py
@@ -1,3 +1,99 @@
+from collections.abc import Iterable
 from django.db import models
-
+from django.contrib.auth.models import User
+from utils.validate import valida_cpf, valida_cep
+from django.core.exceptions import ValidationError
 # Create your models here.
+
+class EnderecoUsuario(models.Model):
+    endereco = models.CharField(max_length=50)
+    numero = models.CharField(max_length=5)
+    complemento = models.CharField(max_length=30)
+    bairro = models.CharField(max_length=30)
+    cep = models.CharField(max_length=8)
+    cidade = models.CharField(max_length=30)
+    estado = models.CharField(
+        default='SP',
+        max_length= 2,
+        choices=(
+            ('AC', 'Acre'),
+            ('AL', 'Alagoas'),
+            ('AP', 'Amapá'),
+            ('AM', 'Amazonas'),
+            ('BA', 'Bahia'),
+            ('CE', 'Ceará'),
+            ('DF', 'Distrito Federal'),
+            ('ES', 'Espírito Santo'),
+            ('GO', 'Goiás'),
+            ('MA', 'Maranhão'),
+            ('MT', 'Mato Grosso'),
+            ('MS', 'Mato Grosso do Sul'),
+            ('MG', 'Minas Gerais'),
+            ('PA', 'Pará'),
+            ('PB', 'Paraíba'),
+            ('PR', 'Paraná'),
+            ('PE', 'Pernambuco'),
+            ('PI', 'Piauí'),
+            ('RJ', 'Rio de Janeiro'),
+            ('RN', 'Rio Grande do Norte'),
+            ('RS', 'Rio Grande do Sul'),
+            ('RO', 'Rondônia'),
+            ('RR', 'Roraima'),
+            ('SC', 'Santa Catarina'),
+            ('SP', 'São Paulo'),
+            ('SE', 'Sergipe'),
+            ('TO', 'Tocantins'),
+        )
+    )
+    
+    
+    def __str__(self):
+        return self.endereco
+    
+    def clean(self):
+        
+        if not valida_cep(self.cep):
+            raise ValidationError({
+                'cep':'cep invalido, tente novamente.'
+            }
+            )
+
+        return super().clean()
+
+class PerfilUsuario(models.Model):
+    
+    class Meta:
+        verbose_name = 'Perfil'
+        verbose_name_plural = 'Perfis'
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    idade = models.PositiveIntegerField()
+    data_nascimento = models.DateField()
+    cpf = models.CharField(max_length=11)
+    endereco = models.ForeignKey(
+        EnderecoUsuario,
+        on_delete=models.CASCADE
+        )
+
+    
+    def __str__(self):
+        if self.user.first_name:
+            return self.user.first_name
+
+        return self.user.username
+    
+    def clean(self):
+        
+        if not valida_cpf(self.cpf):
+            raise ValidationError({
+                'cpf':'cpf invalido, tente novamente.'
+            }
+            )
+
+        return super().clean()
+    
+
+    
+    
+
+    
--- utils/validate.py
+++ utils/validate.py
@@ -1,10 +1,8 @@
-
-
 from pathlib import Path
 
 from django.conf import settings
 from PIL import Image
-
+import re
 
 def resize_image(image_django, new_width=800, optimize=True, quality=60):
     image_path = Path(settings.MEDIA_ROOT / image_django.name).resolve()
@@ -28,3 +26,55 @@ def resize_image(image_django, new_width=800, optimize=True, quality=60):
     )
 
     return new_image
+
+
+def valida_cpf(cpf):
+    cpf = str(cpf)
+    cpf = re.sub(r'[^0-9]', '', cpf)
+
+    if not cpf or len(cpf) != 11:
+        return False
+
+    novo_cpf = cpf[:-2]                 # Elimina os dois últimos digitos do CPF
+    reverso = 10                        # Contador reverso
+    total = 0
+
+    # Loop do CPF
+    for i in range(19):
+        if i > 8:                   # Primeiro índice vai de 0 a 9,
+            i -= 9                  # São os 9 primeiros digitos do CPF
+
+        total += int(novo_cpf[i]) * reverso  # Valor total da multiplicação
+
+        reverso -= 1                    # Decrementa o contador reverso
+        if reverso < 2:
+            reverso = 11
+            d = 11 - (total % 11)
+
+            if d > 9:                   # Se o digito for > que 9 o valor é 0
+                d = 0
+            total = 0                   # Zera o total
+            novo_cpf += str(d)          # Concatena o digito gerado no novo cpf
+
+    # Evita sequencias. Ex.: 11111111111, 00000000000...
+    sequencia = novo_cpf == str(novo_cpf[0]) * len(cpf)
+
+    # Descobri que sequências avaliavam como verdadeiro, então também
+    # adicionei essa checagem aqui
+    if cpf == novo_cpf and not sequencia:
+        return True
+    else:
+        return False
+
+
+def valida_cep(cep):
+    list_number = ['0','1','2', '3', '4', '5', '6', '7', '8', '9']
+    
+    for i in cep:
+        if i not in list_number:
+            return False
+    
+    if len (cep) > 8 or len (cep) < 8 :
+         return False
+     
+    return True
