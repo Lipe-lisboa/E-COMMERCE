d39bac1335056aaa6225e5a87e30339ea13aa164
Filipe <filipelisboaalves@gmail.com>
2024-02-12 19:54:08 -0300

    começando a configurar a view AdicionarAoCarrinho

--- produto/views/produto_views.py
+++ produto/views/produto_views.py
@@ -2,7 +2,9 @@ from django.views.generic.list import ListView
 from django.views.generic import DetailView
 from django.views import View
 from django.http import HttpResponse
-from produto.models import Produto
+from produto.models import Produto, Variacao
+from django.shortcuts import redirect, get_object_or_404
+from django.contrib import messages
 
 class ListaProdutos(ListView):
     model = Produto
@@ -18,7 +20,28 @@ class DetalheProduto(DetailView):
     
 class AdicionarAoCarrinho(View):
     def get(self,*args, **kwargs):
-        return HttpResponse("add car")
+        variacao_id = self.request.GET.get('vid')
+        
+        if not variacao_id:
+            messages.error(
+                self.request,
+                'Produto não enviado'
+            )
+            return redirect('produto:lista')
+        
+        variacao = get_object_or_404(Variacao, id = variacao_id)
+        
+        if not self.request.session.get('carrinho'):
+            self.request.session['carrinho'] = {}
+            self.request.session.save()
+            
+        carrinho = self.request.session.get('carrinho')
+        
+        if variacao_id in carrinho:
+            ...
+        else:
+            ...
+        return HttpResponse(f'{variacao.nome} {variacao.produto}')
     
 class RemoverDoCarrinho(View):
     def get(self, *args, **kwargs):
